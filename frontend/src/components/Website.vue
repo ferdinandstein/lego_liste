0d8e8f25bb168296fdc004102f5d02b4e225c28a
Sebastian Stein <seb.kde@hpfsc.de>
2025-05-12 19:42:20 +0200

    fix types

--- frontend/src/components/Website.vue
+++ frontend/src/components/Website.vue
@@ -12,18 +12,24 @@
       <v-divider class="mb-10"/>
       <SearchResultView :part="currentPart" :searchResult="searchResult"/>
       <Overview :searchResult="searchResult"/>
-      <add-new-set/>
+      <AddNewSet/>
     </div>
   </v-container>
 </template>
 
 <script setup lang="ts">
+import AddNewSet from "@/components/AddNewSet.vue";
+import Overview from "@/components/Overview.vue";
+import SearchBrick from "@/components/SearchBrick.vue";
+import SearchResultView from "@/components/SearchResultView.vue";
 import type { Part } from "@/model/Part";
 import { useSearchResults } from "@/service/SearchService";
 
-const { currentPart, searchResult, loadPartResults } = useSearchResults();
+const {currentPart, searchResult, loadPartResults} = useSearchResults();
 
-const newPartLoaded = async (part: Part) => {
-  await loadPartResults(part);
+const newPartLoaded = async (part: Part | undefined) => {
+  if (part) {
+    await loadPartResults(part);
+  }
 };
 </script>
