7ce6efa14e8f85eff3581d465484a10b51b2d5ac
Sebastian Stein <seb.kde@hpfsc.de>
2025-05-12 19:26:35 +0200

    remove dead code

--- frontend/src/components/Overview.vue
+++ frontend/src/components/Overview.vue
@@ -1,30 +1,26 @@
 <template>
-    <div class="position-absolute top-0 right-0 rounded-lg pa-3">
-        <v-expansion-panels variant="accordion">
-            <v-expansion-panel title="General infos for Part">
-                <v-expansion-panel-text v-if="searchResult">
-                    <v-list-item v-for="quantityPerColor in searchResult.quantityPerColor"
-                        :key="quantityPerColor.colorId">
-                        <h4>
-                            <strong class="text-grey-lighten-1">
-                                {{ quantityPerColor.quantity }} in {{
-                                    getColorInfo(quantityPerColor.colorId).name }}
-                            </strong>
-                        </h4>
-                    </v-list-item>
-                </v-expansion-panel-text>
-            </v-expansion-panel>
-        </v-expansion-panels>
-    </div>
+  <div class="position-absolute top-0 right-0 rounded-lg pa-3">
+    <v-expansion-panels variant="accordion">
+      <v-expansion-panel title="General infos for Part">
+        <v-expansion-panel-text v-if="searchResult">
+          <v-list-item v-for="quantityPerColor in searchResult.quantityPerColor"
+                       :key="quantityPerColor.colorId">
+            <h4>
+              <strong class="text-grey-lighten-1">
+                {{ quantityPerColor.quantity }} in {{ quantityPerColor.colorId }}
+              </strong>
+            </h4>
+          </v-list-item>
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+    </v-expansion-panels>
+  </div>
 </template>
 
 <script setup lang="ts">
 import type { SearchResult } from "@/model/SearchResult";
-import { useColorInfo } from "@/service/ColorService";
 
 defineProps<{
-    searchResult: SearchResult | undefined;
+  searchResult: SearchResult | undefined;
 }>();
-
-const { getColorInfo } = useColorInfo();
 </script>
